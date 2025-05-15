3482c35251557a4737d9ffe7e9db082a811316b2
Muhammad Haikal Siregar <haikalsiregar427@gmail.com>
2025-05-15 23:25:57 +0700

    Add files via upload

---
+++ Pertemuan 7/soal1.js
@@ -0,0 +1,9 @@
+// Problem 1 - String Concatenation
+let firstName = 'Haikal';
+let lastName = 'Siregar';
+let bornYear = 2007;
+
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
---
+++ Pertemuan 7/soal3.js
@@ -0,0 +1,6 @@
+let panjang = 20;
+    let lebar = 5;
+    let luasPersegi = panjang * lebar;
+    let kelilingPersegi = 2 * (panjang + lebar);
+    console.log("Luas persegi Panjang = " + luasPersegi);
+    console.log("Keliling persegi Panjang = " + kelilingPersegi);
